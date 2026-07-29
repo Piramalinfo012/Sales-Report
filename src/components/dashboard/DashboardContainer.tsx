958d1107f37262f2c76cfcafc6da58f5b6e7f256
Piramalinfo012 <info@piramalpetrolium.com>
2026-07-29 15:47:13 +0530

    Location Tracking

--- src/components/dashboard/DashboardContainer.tsx
+++ src/components/dashboard/DashboardContainer.tsx
@@ -93,14 +93,14 @@ export const DashboardContainer: React.FC = () => {
       />
 
       {/* Main Content Area */}
-      <div className="flex-1 flex flex-col min-w-0">
+      <div className="flex-1 flex flex-col min-w-0 min-h-0">
         <Header
           currentTab={currentTab}
           onTabChange={setCurrentTab}
           toggleSidebarMobile={() => setIsOpenMobileSidebar(!isOpenMobileSidebar)}
         />
 
-        <div className="flex-1 overflow-y-auto relative">
+        <div className="flex-1 min-h-0 overflow-y-auto relative">
           <main className="p-4 md:p-6 lg:p-8 max-w-7xl w-full mx-auto pb-12">
             {renderActiveModule()}
           </main>
